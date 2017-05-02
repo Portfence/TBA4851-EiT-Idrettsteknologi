d6630b57e1be8d9b74bd068b10b44a80eb252cd8
Albert Havnegjerde <Albert Havnegjerde>
2017-05-02 16:11:00 +0200

    final commit before deadline

---
+++ isfaktorer.m
@@ -0,0 +1,12 @@
+
+%root[v^3,v^2,v,c]
+optimalFartR=roots([cfLuft 0.000175*personVekt*g personVekt*g*0.0034 -total]);%-1
+optimalFartR=roots([cfLuft 0.000169*personVekt*g personVekt*g*0.003425 -total]);%-2
+optimalFartR=roots([cfLuft 0.0001625*personVekt*g personVekt*g*0.00345 -total]);%-3
+optimalFartR=roots([cfLuft 0.00015625*personVekt*g personVekt*g*0.003475 -total]);%-4
+optimalFartR=roots([cfLuft 0.00015*personVekt*g personVekt*g*0.0035 -total]);%-5
+optimalFartR=roots([cfLuft 0.000146*personVekt*g personVekt*g*0.00352 -total]);%-6
+optimalFartR=roots([cfLuft 0.000143*personVekt*g personVekt*g*0.00354 -total]);%-7
+optimalFartR=roots([cfLuft 0.00014*personVekt*g personVekt*g*0.00356 -total]);%-8
+optimalFartR=roots([cfLuft 0.000136*personVekt*g personVekt*g*0.00358 -total]);%-9
+optimalFartR=roots([cfLuft 0.00013*personVekt*g personVekt*g*0.0036 -total]);%-10
